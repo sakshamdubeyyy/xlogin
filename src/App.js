5034b176596d87a745ba00871545ffc146045de4
sakshamdubeyyy <109180511+sakshamdubeyyy@users.noreply.github.com>
2024-03-04 17:02:59 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { isVisible } from '@testing-library/user-event/dist/utils';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(username === 'user' && password === 'password'){
+      setError('');  
+      setIsSubmitted(true);
+    }else{
+      setError('Invalid username or password');
+      setIsSubmitted(false);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      {isSubmitted ? (
+        <div>
+          <p>Welcome, {username}</p>
+        </div>
+      ):
+      <form onSubmit={handleSubmit} className='form'>
+        {error && <p className={`form-popup ${isVisible ? 'visible' : ''}`}>{error}</p>}
+        <label htmlFor='username'>Username:</label>
+        <input id='username' type='text' value={username} placeholder='Username' onChange={e => setUsername(e.target.value)}/>
+        <label htmlFor='password'>Password:</label>
+        <input id='password' type='password' value={password} placeholder='Password' onChange={e => setPassword(e.target.value)}/>
+        <button type='submit'>Submit</button>
+      </form>
+      }   
     </div>
   );
 }
